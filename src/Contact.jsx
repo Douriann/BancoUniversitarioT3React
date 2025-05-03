32418a5d15058e9ae48595ca9475e169dff64c08
Douriann <scoclox@gmail.com>
2025-05-02 20:10:27 -0400

    colocando archivos del proyecto en react

---
+++ src/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+  return (
+    <section className="contact" id="contact">
+      <h2 className="contact-title">Contáctanos</h2>
+      <p>Dirección: Av. Universidad, Edificio Banco Universitario, piso 12, Caracas, Venezuela</p>
+      <p>Teléfono: +58 212-555-5555 •	Fax: +58 212-555-5556</p>
+      <p>Correo electrónico: info@bancouniversitario.com.ve</p>
+      <p>Redes Sociales: @BancoUniversitariove [Facebook, Instagram, Twitter]</p>
+    </section>
+  );
+}
+
+export default Contact;
