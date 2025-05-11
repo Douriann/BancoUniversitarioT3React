a6d0b518b1a4405387f5777bdf988a330a674d6f
Douriann <scoclox@gmail.com>
2025-05-11 17:00:05 -0400

    Añadiendo lógica al dashboard para adquirir y mostrar datos del usuario logeado / Modificando catch de error en el login

--- src/pages/dashboard/BancaLineaHeader.jsx
+++ src/pages/dashboard/BancaLineaHeader.jsx
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import './BancaLineaHeader.css';
 import logo from '../../assets/img/logo.png';
 import logoutimg from '../../assets/img/logout.png';
+import { getJWT } from "../../utils/localStorage";
+import { Link , useNavigate} from 'react-router-dom';
+import { apiRequest } from "../../api/apirequest";
 
 function BancaLineaHeader() {
+   const navigate = useNavigate();
+   const [userName, setUserName] = useState("Usuario");
+
+    useEffect(() => {
+        const findUserName = async () => {
+            try{
+                const response = await apiRequest("GET", "/v1/client/user/whoami", getJWT);
+                console.log("Respuesta del servidor:", response);
+                setUserName(`${response.data.first_name} ${response.data.last_name}`);
+            }
+            catch (error) {
+                console.error("Error al conectar con el servidor:", error);
+                alert("Error al conectar con el servidor");
+                navigate("/bancalinea/login");
+            }
+        }
+        findUserName();
+    }, []);
   return (
     <header class="header-dashboard">
         <div class="header-left-elements">
             <div class="header-img-container">
                 <img src={logo} alt="Banco Universitario Logo" class="header-logo"/>
             </div>
-            <h2>Bienvenido(a) </h2>
+            <h2>Bienvenido(a) {userName}</h2>
         </div>
         <div class="header-btn-container">
-            <a href="#home" class="btn-header">
+            <Link to="/bancalinea/login" class="btn-header">
                 Salir
                 <img src={logoutimg} alt="Usuario" class="header-btn-img-logout"/>
-            </a>         
+            </Link>         
         </div>
     </header>
   );
