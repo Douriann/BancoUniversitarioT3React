d000c0d706682011222f06e85f19d26d50e3a0ff
Angel A. Goyo <56369175+angel10sai@users.noreply.github.com>
2025-05-14 17:06:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+<<<<<<< Updated upstream
     port: 8000
+=======
+    proxy: {
+      "/api": {
+        port: 8000,
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+>>>>>>> Stashed changes
   }
 })
