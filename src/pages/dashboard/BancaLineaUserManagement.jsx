4c7031db6bfbd8c1fcef379d418909aa45608d78
joseAlv <1001.27524130.ucla@gmail.com>
2025-05-20 17:17:07 -0400

    Agregando Validaciones al formulario y acomodando estilos

--- src/pages/dashboard/BancaLineaUserManagement.jsx
+++ src/pages/dashboard/BancaLineaUserManagement.jsx
@@ -5,15 +5,50 @@ import "./BancaLineaUserManagement.css";
 const UserManagement = () => {
     const [currentPassword, setCurrentPassword] = useState("");
     const [newPassword, setNewPassword] = useState("");
-    const [repeatNewPassword, setRepeatNewPassword] = useState("");    
+    const [repeatNewPassword, setRepeatNewPassword] = useState("");
+    const [error, setError] = useState("") 
 
-    const handleSubmit = (e) => {
-    e.preventDefault();
+    const passwordSecure = (password) => {
+        const securePasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+        return securePasswordRegex.test(password);
+    }
+
+    const handleSubmit = (event) => {
+    event.preventDefault();
+
+    //Bloque de validaciones
+    if (!currentPassword||!newPassword||!repeatNewPassword) {
+        setError("Por favor, complete todos los campos.");
+        return;
+    }
+    if (newPassword !== repeatNewPassword){
+        setError("Las contraseñas nuevas no coinciden.");
+        return;
+    }
+    if (!passwordSecure(newPassword)){
+        setError("La contraseña nueva debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.");
+        return;
+    }
+
+    //Lógica para enviar los datos al servidor
+    setError("");
     alert("Contraseña modificada exitosamente.");
+
+    //Limpiar campos
+    setCurrentPassword("");
+    setNewPassword("");
+    setRepeatNewPassword("");
     };
 
+    const handleCancel = () => {
+    setCurrentPassword("");
+    setNewPassword("");
+    setRepeatNewPassword("");
+    setError("");
+    }
+
     return (
-        <div class="container-mainclass">
+        <div className="container-mainclass">
         <BancaLineaSidebar/>
             <main className="userManagement-main-content">
                 <h2>Gestión de Usuario</h2>
@@ -38,13 +73,17 @@ const UserManagement = () => {
                             type="password"
                             value={repeatNewPassword}
                             onChange={(e) => setRepeatNewPassword(e.target.value)}
-                        />                        
+                        />
 
                         <div className="buttons">
-                            <button type="button" className="cancel">Cancelar</button>
+                            <button type="button" className="cancel" onClick={handleCancel}>Cancelar</button>
                             <button type="submit" className="modify">Modificar</button>
-                        </div>
+                        </div>                        
                     </form>
+
+                    <div className="error-box">
+                        {error && <p className="error-message">{error}</p>}
+                    </div>
                 </div>    
             </main>
         </div>
